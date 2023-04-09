a5f2f2e4870ae2f339f87c9ba4a1bc9f8db058d8
Paulin <contact@paulin-buland.fr>
2023-04-09 16:12:46 +0200

    Ajout du schéma

---
+++ schema/CategorieVitrine.schema.ts
@@ -0,0 +1,20 @@
+//Documentation du schéma de la catégorie de vitrine
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     CategorieVitrine:
+ *       type: object
+ *       required:
+ *         - WORDING
+ *       properties:
+ *         ID_CATEGORY_VITRINE:
+ *           type: int
+ *           description: ID de la catégorie auto-généré
+ *         WORDING:
+ *           type: string
+ *           description: Libellé de la catégorie
+ *       example:
+ *         ID_USER : 1
+ *         WORDING : "Potager"
+ */
