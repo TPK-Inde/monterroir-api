5326974e4304ae3810365937be0ee4f77bed7aa7
Paulin Buland <buland2001@gmail.com>
2023-04-20 09:23:30 +0200

    Avancement mais pas fini

--- tests/categoriesVitrine.test.ts
+++ tests/categoriesVitrine.test.ts
@@ -1,8 +1,28 @@
+import { Response } from "express";
 import request from "supertest";
+import { CategoryVitrine } from "../models/CategoryVitrine";
+import { CategoryVitrineRepository } from "../Lib/Repositories/CategoryVitrineRepository";
+import CategoriesVitrine from "../services/categoriesVitrine";
 const app = require("../app");
 
 describe("Route - Catégories de vitrine", () => {
   test("Récupérer toutes les catégories de vitrine", async () => {
+    const res = await request(app).get("/categoriesVitrine");
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+
+///////////////////////////////////////////
+//Avec Mock Sequelize
+
+// const resultGetOne = jest.spyOn(CategoryVitrineRepository.prototype, 'GetById');
+
+describe("Route - Catégories de vitrine", () => {
+  test("Récupérer une catégorie de vitrine", async () => {
+
+
+
     const res = await request(app).get("/categoriesVitrine");
     expect(res.statusCode).toEqual(200);
   });
