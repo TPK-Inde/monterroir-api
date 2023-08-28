d014db01709d84697f3f9404677262ed2e914629
Paulin Buland <buland2001@gmail.com>
2023-08-28 13:59:47 +0200

    Test unitaire GET Catégorie de vitrine

--- tests/categoriesVitrine.test.ts
+++ tests/categoriesVitrine.test.ts
@@ -1,34 +1,16 @@
-// import { Response } from "express";
-// import request from "supertest";
-// import { CategoryVitrine } from "../models/CategoryVitrine";
-// import { CategoryVitrineRepository } from "../Lib/Repositories/CategoryVitrineRepository";
-// import CategoriesVitrine from "../services/categoriesVitrine";
-// const app = require("../app");
+import request from "supertest";
+const app = require("../app");
 
-// describe("Route - Catégories de vitrine", () => {
-//   test("Récupérer toutes les catégories de vitrine", async () => {
-//     const res = await request(app).get("/categoriesVitrine");
-//     expect(res.statusCode).toEqual(200);
-//   });
-// });
+describe("Route GET - Catégories de vitrine", () => {
+  test("Récupérer toutes les catégories de vitrine", async () => {
+    const res = await request(app).get("/categoriesVitrine");
 
-///////////////////////////////////////////
-//Avec Mock Sequelize
+    expect(res.statusCode).toEqual(200);
+  });
 
-// const resultGetOne = jest.spyOn(CategoryVitrineRepository.prototype, 'GetById');
+  test("Récupérer une catégorie de vitrine", async () => {
+    const res = await request(app).get("/categoriesVitrine/1");
 
-// describe("Route - Catégories de vitrine", () => {
-//   test("Récupérer une catégorie de vitrine", async () => {
-
-
-
-//     const res = await request(app).get("/categoriesVitrine");
-//     expect(res.statusCode).toEqual(200);
-//   });
-// });
-
-describe("Faux test unitaire", () => {
-  test("Faux test unitaire", async () => {
-    expect(true).toEqual(true);
+    expect(res.statusCode).toEqual(200);
   });
 });
