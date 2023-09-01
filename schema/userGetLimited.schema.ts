2cbf3813e0c1cd27cbc6ab715b5b4711160cbb25
Paulin Buland <buland2001@gmail.com>
2023-09-01 09:18:23 +0200

    MT - 0253 : Ajout du statut du compte

--- schema/userGetLimited.schema.ts
+++ schema/userGetLimited.schema.ts
@@ -15,8 +15,21 @@
  *         PROFIL_PICTURE:
  *           type: text
  *           description: Photo de profil de l'utilisateur BASE 64
+ *         ACCOUNT_STATUS:
+ *           type: object
+ *           properties:
+ *             ID_ACCOUNT_STATUS:
+ *               type: int
+ *               description: ID du statut du compte
+ *             WORDING:
+ *               type: string
+ *               description: Libellé du statut du compte
  *       example:
  *         ID_USER : 1
  *         PSEUDONYM: "Marc_Dupond"
  *         PROFIL_PICTURE : "image/2023/12/example.png"
+ *         ACCOUNT_STATUS: {
+ *           ID_ACCOUNT_STATUS: 1,
+ *           WORDING: "Utilisateur"
+ *         }
  */
